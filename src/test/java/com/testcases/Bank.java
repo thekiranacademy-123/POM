6c519fcf7de41ae04838b16cf3fa6bb45dc3035c
thekiranacademy-123 <iranna@thekiranacademy.com>
2025-07-18 12:36:12 +0530

    first commit

---
+++ src/test/java/com/testcases/Bank.java
@@ -0,0 +1,30 @@
+package com.testcases;
+
+import java.time.LocalDateTime;
+
+public class Bank extends Thread {
+
+	public static int balance =1000;
+	@Override
+	public void run()
+	{
+		for(int i=0;i<5;i++)
+		{
+			withdraw(150);
+		}
+	}
+	public static synchronized void withdraw(int amt)
+	{
+		if(balance>=150)
+		{
+			System.out.println(Thread.currentThread().getName());
+			balance =balance-amt;
+			System.out.println("Remaining balance ="+ balance );
+			System.out.println(LocalDateTime.now());
+		}else
+		{
+			System.out.println("insuffient balance ");
+		}
+		
+	}
+}
