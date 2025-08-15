a0e6251e64f549dd0262510d18a3e6685a621a98
thekiranacademy-123 <iranna@thekiranacademy.com>
2025-08-15 19:15:42 +0530

    changes in Employee file at login_feature branch

---
+++ src/test/java/com/testcases/Employee.java
@@ -0,0 +1,14 @@
+package com.testcases;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+public class Employee {
+	@Test
+	void test()
+	{
+		System.out.println("Tst Excution Started");
+		Assert.assertEquals("Hello", "Hello");
+	}
+}
